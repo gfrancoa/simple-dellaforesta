fdf19fdf305f15dcde6ba8bdfb3ec74fa6b652d3
gfrancoa <gfrancoa@outlook.com>
2021-02-08 11:39:42 -0500

    versión final de sitio web sencillo. Hecho como desafío de la escuela Holberton

---
+++ behavior.js
@@ -0,0 +1,97 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+
+    thumbnailElement.addEventListener("click", function () {
+        thumbnailElement.className = "";
+
+        thumbnailElement.addEventListener("click", function () {
+
+            if (thumbnailElement.className == "") {
+                thumbnailElement.className = "small";
+            }
+            else {
+                thumbnailElement.className == ""
+            }
+        });
+
+    });
+
+    var thumbnailElement2 = document.getElementById("smart_thumbnail2");
+
+    thumbnailElement2.addEventListener("click", function () {
+        thumbnailElement2.className = "";
+
+        thumbnailElement2.addEventListener("click", function () {
+
+            if (thumbnailElement2.className == "") {
+                thumbnailElement2.className = "small";
+            }
+        });
+
+    });
+
+    var thumbnailElement3 = document.getElementById("smart_thumbnail3");
+
+    thumbnailElement3.addEventListener("click", function () {
+        thumbnailElement3.className = "";
+
+        thumbnailElement3.addEventListener("click", function () {
+
+            if (thumbnailElement3.className == "") {
+                thumbnailElement3.className = "small";
+            }
+        });
+
+    });
+
+    var thumbnailElement4 = document.getElementById("smart_thumbnail4");
+
+    thumbnailElement4.addEventListener("click", function () {
+        thumbnailElement4.className = "";
+
+        thumbnailElement4.addEventListener("click", function () {
+
+            if (thumbnailElement4.className == "") {
+                thumbnailElement4.className = "small";
+            }
+        });
+
+    });
+
+    var thumbnailElement5 = document.getElementById("smart_thumbnail5");
+
+    thumbnailElement5.addEventListener("click", function () {
+        thumbnailElement5.className = "";
+
+        thumbnailElement5.addEventListener("click", function () {
+
+            if (thumbnailElement5.className == "") {
+                thumbnailElement5.className = "small";
+            }
+        });
+
+    });
+
+    var thumbnailElement6 = document.getElementById("smart_thumbnail6");
+
+    thumbnailElement6.addEventListener("click", function () {
+        thumbnailElement6.className = "";
+
+        thumbnailElement6.addEventListener("click", function () {
+
+            if (thumbnailElement6.className == "") {
+                thumbnailElement6.className = "small";
+            }
+        });
+
+    });
+
+
+
+
+
+
+
+
+});
